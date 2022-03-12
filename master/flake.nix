fcf48fd259fabe4d75f4d4727ef7e48054802bb0
hugosenari <hugosenari@gmail.com>
2022-03-12 04:29:58 -0300

    chore: re index nimodpi

---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''oracle odpi-c wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-nimodpi-master.flake = false;
+  inputs.src-nimodpi-master.owner = "mikra01";
+  inputs.src-nimodpi-master.ref   = "refs/heads/master";
+  inputs.src-nimodpi-master.repo  = "nimodpi";
+  inputs.src-nimodpi-master.type  = "github";
+  
+  inputs."nimterop".dir   = "nimpkgs/n/nimterop";
+  inputs."nimterop".owner = "riinr";
+  inputs."nimterop".ref   = "flake-pinning";
+  inputs."nimterop".repo  = "flake-nimble";
+  inputs."nimterop".type  = "github";
+  inputs."nimterop".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."nimterop".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-nimodpi-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-nimodpi-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
